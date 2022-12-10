ed0df5cfc7bed79d714d4b2a7636f028b67f8923
somia213 <somiaamer8@gmail.com>
2022-12-10 22:11:51 +0200

    add validation forr form

---
+++ JS/form.js
@@ -0,0 +1,43 @@
+let input1 = document.querySelector(".input1");
+let input2 = document.querySelector(".input2");
+let btn = document.querySelector(".btn");
+let divFourth = document.querySelector(".divFourth");
+
+btn.onclick = function () {
+  if (input1.value != "" && input2.value != "") {
+    let Regex1 = new RegExp(/^\w+([\.-]?\w+)@\w+([\.-]?\w+)(\.\w{2,3})+$/);
+    let Regex2 = new RegExp("(?=.d)(?=.[a-z])(?=.*[A-Z]).{8,}");
+    if (Regex1.test(input1.value) && Regex2.test(input2.value)) {
+      divFourth.innerHTML = `
+                    <p class="p">Welcome</p>
+                `;
+    } else {
+      if (
+        Regex1.test(input1.value) == false &&
+        Regex2.test(input2.value) == true
+      ) {
+        console.log("1f 2t");
+        divFourth.innerHTML = `
+                        <p class="p">Please enter a valid E-Mail</p>
+                    `;
+      } else if (
+        Regex1.test(input1.value) == true &&
+        Regex2.test(input2.value) == false
+      ) {
+        console.log("1t 2f");
+        divFourth.innerHTML = `
+                        <p class="p">Please type your password as a combination of 8 uppercase and lowercase letters and numbers</p>
+                    `;
+      } else {
+        console.log("1f 2f");
+        divFourth.innerHTML = `
+                        <p class="p">Please enter a valid E-Mail and password</p>
+                    `;
+      }
+    }
+  } else {
+    divFourth.innerHTML = `
+        <p class="p">Please enter your E-Mail and password</p>
+    `;
+  }
+};
